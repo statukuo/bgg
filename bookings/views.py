59ec1d534ee22814f27f0cf9bdd7b715cbe2185d
David Barbero <dbdstk@gmail.com>
2022-04-21 16:30:19 +0200

    Add assist and forget actions

--- bookings/views.py
+++ bookings/views.py
@@ -4,27 +4,45 @@ from django.contrib.auth.models import Group, User
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [TokenAuthentication, BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class GroupViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [TokenAuthentication, BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 class TableViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [TokenAuthentication, BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
     queryset = Table.objects.all()
     serializer_class = TableSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authentication_classes = [TokenAuthentication, BasicAuthentication]
+    #permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    @action(detail=True, methods=['post'])
+    def assist(self, request, pk=None):
+        booking = self.get_object()
+        print(booking)
+        print(request.user.id)
+        booking.attendants.add(request.user)
+        booking.save()
+        return Response({})
+    @action(detail=True, methods=['post'])
+    def forget(self, request, pk=None):
+        booking = self.get_object()
+        print(booking)
+        print(request.user.id)
+        booking.attendants.remove(request.user)
+        booking.save()
+        return Response({})
