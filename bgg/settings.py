a98af121083a07f0b0e44407c63968c332314199
David Barbero <dbdstk@gmail.com>
2022-04-22 10:58:06 +0200

    Add auth and current user retrieve

--- bgg/settings.py
+++ bgg/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:3000',
+)
 
 ROOT_URLCONF = 'bgg.urls'
 
--- bookings/views.py
+++ bookings/views.py
@@ -8,41 +8,45 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
-    #authentication_classes = [TokenAuthentication, BasicAuthentication]
-    #permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    @action(detail=False, methods=['get'])
+    def current(self, request, pk=None):
+        return Response({
+            "id": request.user.id,
+            "alias": request.user.member.alias if request.user.member else request.user.username,
+            "email": request.user.email,
+            "username": request.user.username
+        })
 
 class GroupViewSet(viewsets.ModelViewSet):
-    #authentication_classes = [TokenAuthentication, BasicAuthentication]
-    #permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 class TableViewSet(viewsets.ModelViewSet):
-    #authentication_classes = [TokenAuthentication, BasicAuthentication]
-    #permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Table.objects.all()
     serializer_class = TableSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
-    #authentication_classes = [TokenAuthentication, BasicAuthentication]
-    #permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     @action(detail=True, methods=['post'])
     def assist(self, request, pk=None):
         booking = self.get_object()
-        print(booking)
-        print(request.user.id)
         booking.attendants.add(request.user)
         booking.save()
         return Response({})
     @action(detail=True, methods=['post'])
     def forget(self, request, pk=None):
         booking = self.get_object()
-        print(booking)
-        print(request.user.id)
         booking.attendants.remove(request.user)
         booking.save()
         return Response({})
